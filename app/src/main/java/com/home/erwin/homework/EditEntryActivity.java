343773fde66f8e0c295e384234db4d6eb7f7db31
Erwin <erwintran0@gmail.com>
2016-09-27 22:06:00 +0200

    Implemented new Activity
    Implemented EditEntryActivity for editing or adding entries.
    Customized color and strings .xml

---
+++ app/src/main/java/com/home/erwin/homework/EditEntryActivity.java
@@ -0,0 +1,13 @@
+package com.home.erwin.homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_entry_activity);
+    }
+}
--- app/src/main/java/com/home/erwin/homework/MainActivity.java
+++ app/src/main/java/com/home/erwin/homework/MainActivity.java
@@ -1,13 +1,34 @@
 package com.home.erwin.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button newEntryBt;
+    final static int CREATE_NEW_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeComponents();
+        initializeListeners();
+    }
+
+    private void initializeComponents() {
+        newEntryBt = (Button) findViewById(R.id.newEntryBt);
+    }
+
+    private void initializeListeners() {
+        newEntryBt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), EditEntryActivity.class);
+                startActivityForResult(intent, CREATE_NEW_ACTIVITY_REQUEST_CODE);
+            }
+        });
     }
 }
