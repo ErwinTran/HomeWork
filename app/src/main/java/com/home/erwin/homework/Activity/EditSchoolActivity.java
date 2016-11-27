64f6d2faecb1935b09d6e1b54e06fa29fc72e2be
Erwin <erwintran0@gmail.com>
2016-11-27 15:11:18 +0100

    Added dialog for new school classes

--- app/src/main/java/com/home/erwin/homework/Activity/EditSchoolActivity.java
+++ app/src/main/java/com/home/erwin/homework/Activity/EditSchoolActivity.java
@@ -1,16 +1,23 @@
 package com.home.erwin.homework.Activity;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.home.erwin.homework.Model.Weekday;
 import com.home.erwin.homework.R;
@@ -25,6 +32,8 @@ public class EditSchoolActivity extends AppCompatActivity {
     private Button colorPickerBt;
     private Button cancelBt;
     private Button saveBt;
+    private Button addBt;
+    private ListView schoolClassesLv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,8 @@ public class EditSchoolActivity extends AppCompatActivity {
         colorPickerBt = (Button) findViewById(R.id.colorPickerBt);
         cancelBt = (Button) findViewById(R.id.cancelBt);
         saveBt = (Button) findViewById(R.id.saveBt);
+        addBt = (Button) findViewById(R.id.addClassBt);
+        schoolClassesLv = (ListView) findViewById(R.id.schoolClassesLv);
     }
 
     private void initializeListeners() {
@@ -64,6 +75,40 @@ public class EditSchoolActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        addBt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showAddClassDialog();
+            }
+        });
+    }
+
+    private void showAddClassDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_add_class);
+        dialog.setTitle("New class");
+
+        EditText inputEt = (EditText) findViewById(R.id.inputClassName);
+
+        Button cancelBt = (Button) dialog.findViewById(R.id.cancelBt);
+        cancelBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        Button saveBt = (Button) dialog.findViewById(R.id.saveBt);
+        saveBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // save data
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 
     private void initializeSpinner() {
@@ -79,5 +124,23 @@ public class EditSchoolActivity extends AppCompatActivity {
         ArrayAdapter<String> weekdayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, weekdayList);
         weekdayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         schoolDaySp.setAdapter(weekdayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.delete, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_delete:
+
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
